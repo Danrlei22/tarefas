2c30f12e9f0089ddee1bfc4abf1b1d4dee9f4bf8
unknown <danrlei_vieira@hotmail.com>
2025-02-28 14:20:06 -0300

    refactor: improving stylization for better website visibility

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -17,15 +17,16 @@ const TodoForm = ({ onSubmit }) => {
   return (
     <>
       <div className={styles.todoHeader}>
-        <h1>Tarefas</h1>
+        <h1 className={styles.titleH1}>Tarefas</h1>
       </div>
       <div className={styles.formTodo}>
-        <h2>Insira sua próxima tarefa:</h2>
+        <h2 className={styles.titleH2}>Insira sua próxima tarefa:</h2>
       </div>
       <form onSubmit={handleSubmit}>
         <div className={styles.formControl}>
-          <label htmlFor="title">Tarefa:</label>
+          <label className={styles.labelTask} htmlFor="title">Tarefa:</label>
           <input
+          className={styles.inputTask}
             type="text"
             id="title"
             placeholder="Título da tarefa"
@@ -36,8 +37,9 @@ const TodoForm = ({ onSubmit }) => {
         </div>
 
         <div className={styles.formControl}>
-          <label htmlFor="time">Duração:</label>
+          <label className={styles.labelTask} htmlFor="time">Duração:</label>
           <input
+           className={styles.inputTime}
             type="text"
             id="time"
             placeholder="Tempo estimado (em horas)"
